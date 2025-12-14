0ee8725272205e2343d74116a42fff423c060cf3
Aryansolanki9917 <asnish9917@gmail.com>
2025-12-14 06:26:54 +0530

    Create Model.js

---
+++ Model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const donationSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  amount: Number,
+  message: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Donation", donationSchema);
